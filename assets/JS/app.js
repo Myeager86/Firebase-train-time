595b792aef60ca0f5459060f8b8ed603f43751ea
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-03-25 12:05:01 -0600

    small revisions

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -18,9 +18,7 @@ $(document).ready(function () {
     var destination = '';
     var firstTime = '';
     var frequency = 0;
-    // var CurrentDate = moment().format();
-    // var totalBilled = 0;
-    // var monthsWorked = 0;
+
 
     // submit button
     $('#submit-button').on('click', function (event) {
@@ -32,24 +30,14 @@ $(document).ready(function () {
         firstTime = $('#firstTime').val().trim();
         frequency = $('#frequency').val().trim();
 
-        
-        // var newDate = $('#start-date').val().trim();
-        // newDate = new Date(newDate);
-        // var today = new Date();
-        // var dif = today - newDate;
-        // //2629743 seconds in a month(on average), divided by 1000 ms in a second
-        // var monthsWorked = Math.floor((dif / 2629743) / 1000);
 
-        // totalBilled = Math.round(monthsWorked * frequency);
 
         database.ref().push({
             name: name,
             destination: destination,
             firstTime: firstTime,
             frequency: frequency,
-            // monthsWorked: monthsWorked,
-            // totalBilled: totalBilled,
-            // dateAdded: firebase.database.ServerValue.TIMESTAMP
+            
         });
 
         name = $('#name').val("");
@@ -66,7 +54,7 @@ $(document).ready(function () {
 
         var newRow = $('<tr>');
 
-        var trainTime = moment().format($('#firstTime').val().trim());
+        var trainTime = moment().format($('#firstTime').val().trim(), "HH:mm");
 
         // trainTime = $('#firstTime').val().trim();
 
@@ -74,8 +62,7 @@ $(document).ready(function () {
         newRow.append('<td>' + sv.destination + '</td>');
         newRow.append('<td>' + sv.frequency + '</td>');
         // newRow.append('<td>' + sv.firstTime + '</td>');
-        // newRow.append('<td>' + sv.monthsWorked + '</td>');
-        // newRow.append('<td>' + sv.totalBilled + '</td>');
+
 
         $('.table-data').append(newRow);
 
@@ -83,8 +70,7 @@ $(document).ready(function () {
         console.log(sv.destination);
         console.log(sv.firstTime);
         console.log(sv.frequency);
-        // console.log(CurrentDate);
-        console.log(moment(trainTime, "HH:mm").format());
+        console.log(moment(trainTime, "HH:mm").format("H HH"));
         // console.log(trainTime);
     });
 
